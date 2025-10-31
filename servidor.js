ccc4a8f5935ccd209c1a8f48f5c7fde73762e089
esantos6pag <esantos6@uolinc.com>
2025-10-31 17:52:37 -0300

    alteração de serv

--- servidor.js
+++ servidor.js
@@ -1,5 +1,5 @@
 app = require('./src/configuracoes/custom-express');
 
 app.listen(process.env.PORT || 3000,()=>{
-    console.log('Servidor esta sendo executado na porta 3000!');
+    console.log('Servidor local esta sendo executado na porta 3000!');
 })
